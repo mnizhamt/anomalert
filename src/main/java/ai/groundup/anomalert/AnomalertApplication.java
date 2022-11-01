13d82fea81e0cfae3cd911ce9a20e147b6db131e
Nizham Tanjung <nizhamt@gmail.com>
2022-11-01 11:09:03 +0700

    Initial Codes

---
+++ src/main/java/ai/groundup/anomalert/AnomalertApplication.java
@@ -0,0 +1,13 @@
+package ai.groundup.anomalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnomalertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnomalertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai/groundup/anomalert/AnomalertApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.groundup.anomalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnomalertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
